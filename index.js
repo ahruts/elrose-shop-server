27acefa50dfc51993d00dc3fe07b392c63364561
Yelyzaveta Ahruts <agruts1997@gmail.com>
2022-04-27 22:12:31 +0300

    first commit

--- index.js
+++ index.js
@@ -4,37 +4,18 @@ let products = require('./index.json')
 const app = express();
 app.use(express.json())
 
-// const users = [
-//   { id: 4, email: 'qwe@qwe.com', password: '123123' },
-//   { id: 7, email: 'asd@asd.com', password: '321321' }
-// ]
-
 const port = process.env.PORT || 8085;
-
-app.post('/api/login', (req, res) => {
-  const { email, password } = req.body
-  const isValid = users.some(user => user.email === email && user.password === password);
-  setTimeout(() => {
-     if (isValid) {
-        const user = users.find(user => user.email)
-        res.json(user)
-     } else {
-        res.status(403)
-        res.json({ message: ['Incorrect email or password', 'Password should contain number'] })
-     }
-  }, 2000)
-})
 
 app.get('/api/products', (req, res) => {
   res.json(products);
 })
 
-// app.get('/api/emails/:emailId', (req, res) => {
-//   const { emailId } = req.params;
-//   const email = emails.find(email => email.id === +emailId)
-//   if (!email) res.status(404).json({ message: 'Email not found' })
-//   res.json(email);
-// })
+app.get('/api/products/:productID', (req, res) => {
+  const { productID } = req.params;
+  const email = emails.find((email) => email.id === +productID);
+  if (!email) res.status(404).json({ message: 'Product not found' })
+  res.json(email);
+})
 
 // app.put('/api/emails/:emailId', (req, res) => {
 //   const { emailId } = req.params;
